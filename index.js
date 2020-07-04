1f79c585d5e3976b441655fc22b63b8cc864b9fd
Marconi Gomes <mgrf@cin.ufpe.br>
2020-07-03 22:11:13 -0300

    Adding support to legacy searches.

--- index.js
+++ index.js
@@ -10,7 +10,6 @@
 const ytsr = require('ytsr');
 const winston = require('winston');
 var YTGBss = require('express')();
-var bodyParser = require('body-parser');
 var http = require('http').createServer(YTGBss);
 
 
@@ -21,7 +20,7 @@ const logger = winston.createLogger({
   level: 'http',
   format: winston.format.prettyPrint(),
   transports: [
-    new winston.transports.File({ filename: 'status.log'}),
+    new winston.transports.File({ filename: 'status.log' }),
     new winston.transports.Console()
   ]
 });;
@@ -31,44 +30,12 @@ const logger = winston.createLogger({
  * Listens for connections on port 54522.
  */
 http.listen(54522, () => {
-  logger.log({timestamp: new Date().toUTCString(), level: 'info', message: 'Ready.'});
+  logger.log({ timestamp: new Date().toUTCString(), level: 'info', message: 'Ready.' });
 });
 
 
 /**
- * Parses the bodyrequest body into JSON format.
- */
-YTGBss.use(bodyParser.json());
-
-
-/**
- * Handles POST requests on /search route.
- * It makes the search by the term available on the request body.
- *
- * We can expect the following results:
- * 200 OK - The search was sucessful. The result will be returned to requester.
- * 400 BAD REQUEST - The request body wasn't in the expected format. The status code is returned.
- * 500 INTERNAL SERVER ERROR - Something went wrong with search. The status code is returned.
- */
-YTGBss.post('/search', (request, response, next) => {
-  logger.log({timestamp: new Date().toUTCString(), level: 'http', message: 'Got POST search request...'});
-
-  if (request.body.term !== undefined) {
-    handleSearch(request.body.term).then( function(parsedResults) {
-      logger.log({level: 'info', message: 'Sucess.'})
-      response.send(parsedResults);
-    }).catch( function(errorData) {
-      next({message: '500 INTERNAL SERVER ERROR', details: errorData});
-    });
-  }
-  else {
-    next({message: '400 BAD REQUEST', details: 'Missing or malfunct body.'});
-  }
-});
-
-
-/**
- * Handles GET requests on /search route.
+ * Handles GET requests on /current/search route.
  * It makes the search by the term available on the request parameters.
  *
  * We can expect the following results:
@@ -76,19 +43,45 @@ YTGBss.post('/search', (request, response, next) => {
  * 400 BAD REQUEST - The request term wasn't found in the url. The status code is returned.
  * 500 INTERNAL SERVER ERROR - Something went wrong with search. The status code is returned.
  */
-YTGBss.get('/search/:term', (request, response, next) => {
-  logger.log({timestamp: new Date().toUTCString(), level: 'http', message: 'Got GET search request...'});
+YTGBss.get('/current/search/:term', (request, response, next) => {
+  logger.log({ timestamp: new Date().toUTCString(), level: 'http', message: 'Got GET search request...' });
 
   if (request.params.term !== undefined) {
-    handleSearch(request.params.term).then( function(parsedResults) {
-      logger.log({level: 'info', message: 'Sucess.'})
+    handleLegacySearch(request.params.term).then(function (parsedResults) {
+      logger.log({ level: 'info', message: 'Sucess.' })
       response.send(parsedResults);
-    }).catch( function(errorData) {
-      next({message: '500 INTERNAL SERVER ERROR', details: errorData});
+    }).catch(function (errorData) {
+      next({ message: '500 INTERNAL SERVER ERROR', details: errorData });
     });
   }
   else {
-    next({message: '400 BAD REQUEST', details: 'Missing request parameter.'});
+    next({ message: '400 BAD REQUEST', details: 'Missing request parameter.' });
+  }
+});
+
+
+/**
+ * Handles legacy GET requests on /search route.
+ * It makes the search by the term available on the request parameters.
+ *
+ * We can expect the following results:
+ * 200 OK - The search was sucessful. The result will be returned to requester.
+ * 400 BAD REQUEST - The request body wasn't in the expected format. The status code is returned.
+ * 500 INTERNAL SERVER ERROR - Something went wrong with search. The status code is returned.
+ */
+YTGBss.get('/search/:term', (request, response, next) => {
+  logger.log({ timestamp: new Date().toUTCString(), level: 'http', message: 'Got LEGACY GET search request...' });
+
+  if (request.params.term !== undefined) {
+    handleSearch(request.params.term).then(function (parsedResults) {
+      logger.log({ level: 'info', message: 'Sucess.' })
+      response.send(parsedResults);
+    }).catch(function (errorData) {
+      next({ message: '500 INTERNAL SERVER ERROR', details: errorData });
+    });
+  }
+  else {
+    next({ message: '400 BAD REQUEST', details: 'Missing or malfunct body.' });
   }
 });
 
@@ -98,24 +91,23 @@ YTGBss.get('/search/:term', (request, response, next) => {
  */
 YTGBss.use(errorHandler);
 
-
 /**
  * Handles the errors provided by the server, answering them accordingly to the client.
  */
 function errorHandler(error, request, response, next) {
-  logger.log({timestamp: new Date().toUTCString(), level: 'error', message: error.details})
+  logger.log({ timestamp: new Date().toUTCString(), level: 'error', message: error.details })
 
   if (error.message.startsWith('500')) {
-    response.status(500).send({error: 'Internal Server Error!'})
-  } 
+    response.status(500).send({ error: 'Internal Server Error!' })
+  }
   else {
-    response.status(400).send({error: 'Bad request!'})
+    response.status(400).send({ error: 'Bad request!' })
   }
 }
 
 
 /**
- * Searchs for YouTube's videos results using node-ytsr lib.
+ * Searchs for YouTube's videos and playlists results using node-ytsr lib.
  *
  * @param {string} term The term to search results for.
  */
@@ -125,24 +117,52 @@ function handleSearch(term) {
     let pageRef = "https://www.youtube.com/results?search_query=" + term;
 
     var searchOptions = {
-        limit: 10,
-        nextpageRef: pageRef
-      };
+      limit: 10,
+      nextpageRef: pageRef
+    };
 
-    doSearch(searchOptions).then( function(videoResults) {
-      for(let videoResult of videoResults) {
+    doSearch(searchOptions).then(function (videoResults) {
+      for (let videoResult of videoResults) {
         finalResults.push(videoResult);
       }
 
-    resolve(finalResults);
-    }).catch( function(errorData) {
+      resolve(finalResults);
+    }).catch(function (errorData) {
+      reject(errorData);
+    });
+  });
+}
+
+
+/**
+ * Searchs for YouTube's videos results using node-ytsr lib.
+ *
+ * @param {string} term The term to search results for.
+ */
+function handleLegacySearch(term) {
+  return new Promise((resolve, reject) => {
+    let finalResults = [];
+    let videoRef = "https://www.youtube.com/results?search_query=" + term + "&sp=EgIQAQ%253D%253D";
+
+    var searchOptions = {
+      limit: 5,
+      nextpageRef: videoRef
+    };
+
+    doLegacySearch(searchOptions).then(function (videoResults) {
+      for (let videoResult of videoResults) {
+        finalResults.push(videoResult);
+      }
+
+      resolve(finalResults);
+    }).catch(function (errorData) {
       reject(errorData);
     });
   });
 }
 
 function doSearch(searchOptions) {
-  return new Promise( function (resolve, reject) {
+  return new Promise(function (resolve, reject) {
     ytsr(null, searchOptions, function (err, searchResults) {
       if (err) {
         reject(err);
@@ -155,12 +175,51 @@ function doSearch(searchOptions) {
 }
 
 
+function doLegacySearch(searchOptions) {
+  return new Promise(function (resolve, reject) {
+    ytsr(null, searchOptions, function (err, searchResults) {
+      if (err) {
+        reject(err);
+      };
+
+      let parsedResults = parseLegacyResults(searchResults.items);
+      resolve(parsedResults);
+    });
+  })
+}
+
+
 /**
  * Parses and returns the search results with necessary information used by YTGBO.
  *
  * @param {Array} results
  */
 function parseResults(results) {
+  var parsedResults = [];
+
+  for (let result of results) {
+    var parsed = {};
+
+    if (result.type == "video" || result.type == "playlist") {
+      parsed.mediaType = result.type;
+      parsed.mediaTitle = result.title;
+      parsed.channelTitle = result.author.name;
+      parsed.mediaUrl = result.link;
+
+      parsedResults.push(parsed);
+    }
+  }
+
+  return parsedResults;
+}
+
+
+/**
+ * Parses and returns the search results with necessary information used by YTGBOs Legacy Version.
+ *
+ * @param {Array} results
+ */
+function parseLegacyResults(results) {
   var parsedResults = [];
 
   for (let result of results) {
