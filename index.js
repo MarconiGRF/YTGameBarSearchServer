bad2cefa27f35a1e6098e6bf8366d2a7b11f61fb
Marconi Gomes <marconi.gomes7@gmail.com>
2020-12-01 09:28:16 -0300

    Rolling back to ytsr engine - wip adjustments

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 
 const { connect } = require('http2');
 /**
- * This file represents Youtube Game Bar Overlay's Search Server. It anonymously makes searches on YouTube 
+ * This file represents Youtube Game Bar Overlay's Search Server. It anonymously makes searches on YouTube
  * by the given term using TimeForANinja's node-ytsr lib, parsing its results to the minimal useful JSON
  * which will be returned to YTGBO.
  *
  * @author: Marconi Gomes (marconi.gomes7@gmail.com)
  */
 
-const youtube = require('scrape-youtube').default;
+const ytsr = require('ytsr');
 const winston = require('winston');
 var YTGBss = require('express')();
 var http = require('http').createServer(YTGBss);
@@ -18,12 +18,12 @@ var http = require('http').createServer(YTGBss);
  * Creates a Winston Logger instance.
  */
 const logger = winston.createLogger({
-  level: 'http',
-  format: winston.format.prettyPrint(),
-  transports: [
-    new winston.transports.File({ filename: 'status.log' }),
-    new winston.transports.Console()
-  ]
+    level: 'http',
+    format: winston.format.prettyPrint(),
+    transports: [
+        new winston.transports.File({ filename: 'status.log' }),
+        new winston.transports.Console()
+    ]
 });
 
 
@@ -31,7 +31,7 @@ const logger = winston.createLogger({
  * Listens for connections on port 54522.
  */
 http.listen(54522, () => {
-  logger.log({ timestamp: new Date().toUTCString(), level: 'info', message: 'Ready.' });
+    logger.log({ timestamp: new Date().toUTCString(), level: 'info', message: 'Ready.' });
 });
 
 
@@ -45,19 +45,19 @@ http.listen(54522, () => {
  * 500 INTERNAL SERVER ERROR - Something went wrong with search. The status code is returned.
  */
 YTGBss.get('/current/search/:term', (request, response, next) => {
-  logger.log({ timestamp: new Date().toUTCString(), level: 'http', message: 'Got GET search request...' });
+    logger.log({ timestamp: new Date().toUTCString(), level: 'http', message: 'Got GET search request...' });
 
-  if (request.params.term !== undefined) {
-    handleSearch(request.params.term).then(function (parsedResults) {
-      logger.log({ level: 'info', message: 'Sucess.' })
-      response.send(parsedResults);
-    }).catch(function (errorData) {
-      next({ message: '500 INTERNAL SERVER ERROR', details: errorData });
-    });
-  }
-  else {
-    next({ message: '400 BAD REQUEST', details: 'Missing request parameter.' });
-  }
+    if (request.params.term !== undefined) {
+        handleSearch(request.params.term).then(function (parsedResults) {
+            logger.log({ level: 'info', message: 'Sucess.' })
+            response.send(parsedResults);
+        }).catch(function (errorData) {
+            next({ message: '500 INTERNAL SERVER ERROR', details: errorData });
+        });
+    }
+    else {
+        next({ message: '400 BAD REQUEST', details: 'Missing request parameter.' });
+    }
 });
 
 
@@ -68,21 +68,21 @@ YTGBss.use(errorHandler);
 
 /**
  * Handles the errors provided by the server, answering them accordingly to the client.
- * 
+ *
  * @param {*} error The error received from above layers.
  * @param {*} request The source request.
  * @param {*} response The response to be made.
  * @param {*} next The next function to be used.
  */
 function errorHandler(error, request, response, next) {
-  logger.log({ timestamp: new Date().toUTCString(), level: 'error', message: error.details })
+    logger.log({ timestamp: new Date().toUTCString(), level: 'error', message: error.details })
 
-  if (error.message.startsWith('500')) {
-    response.status(500).send({ error: 'Internal Server Error!' })
-  }
-  else {
-    response.status(400).send({ error: 'Bad request!' })
-  }
+    if (error.message.startsWith('500')) {
+        response.status(500).send({ error: 'Internal Server Error!' })
+    }
+    else {
+        response.status(400).send({ error: 'Bad request!' })
+    }
 }
 
 /**
@@ -91,29 +91,31 @@ function errorHandler(error, request, response, next) {
  * @param {Array} results
  */
 function parseResults(results) {
-  var parsedResults = [];
-  var itemLimit = 10;
+    var parsedResults = [];
 
-  for(let i = 0; i < itemLimit; i++) {
-    let parsed = {}
-    parsed.mediaType = "video";
-    parsed.mediaTitle = results[i].title;
-    parsed.channelTitle = results[i].channel.name;
-    parsed.mediaUrl = results[i].link;
-
-    parsedResults.push(parsed)
-  }
-  return parsedResults;
+    results.forEach(result => {
+       let parsed = {
+           mediaType: "video",
+           mediaTitle: result.title,
+           mediaUrl: result.link,
+           author: result.author.name
+       };
+       parsedResults.push(parsed);
+    });
+    return parsedResults;
 }
 
 /**
  * Handles the search process by the given term.
- * @param {*} term 
+ * @param {*} term
  */
 const handleSearch = async function(term) {
-    term = term.split(" ").join("");
-    var searchResults = await youtube.search(term);
-    
-    var finalResults = parseResults(searchResults.videos)
+    const filters = await ytsr.getFilters(term);
+    const videoFilter = filters.get('Type').find(obj =>  obj.name === 'Video');
+    const options = { limit: 7 };
+
+    const searchResults = await ytsr(videoFilter.ref, options);
+
+    let finalResults = parseResults(searchResults.items);
     return finalResults;
 }
